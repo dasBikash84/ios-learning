13c4f702aa73c6db19a249c4371067920bccfaaa
Bikash das <das.bikash.dev@gmail.com>
2020-05-17 16:10:56 +0600

    Dice app completed.

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -12,11 +12,37 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceImages = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+    
+    var leftPosition = 0
+    var rightPosition = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        refreshDices()
+        showRandDice()
+        diceImageView2.alpha = 0.75
     }
 
-
+    @IBAction func buttonRoll(_ sender: UIButton) {
+//        refreshDices()
+        showRandDice()
+    }
+    
+    func refreshDices(){
+        leftPosition += 1
+        rightPosition += 1
+        print("leftPosition: \(leftPosition) rightPosition: \(rightPosition)")
+        diceImageView1.image = diceImages[leftPosition%6]
+        diceImageView2.image = diceImages[5 - (leftPosition%6)]
+        
+    }
+    
+    func showRandDice(){
+        diceImageView1.image = diceImages[Int.random(in: 0...5)]
+        diceImageView2.image = diceImages[Int.random(in: 0...5)]
+    }
 }
 
