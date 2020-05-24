eba3931ba855771ff06163c29201cc4be7077ec3
bikash <bikash@bikashs-iMac.local>
2020-05-24 09:11:49 +0600

    clima project added and text input field fnctinality added.

---
+++ Clima/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController,UITextFieldDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var searchText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchText.delegate = self
+    }
+
+    @IBAction func searchTextEntered(_ sender: UITextField) {
+        print("searchTextEntered")
+        print(searchText.text!)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("textFieldDidBeginEditing")
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("textFieldDidEndEditing")
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchText.endEditing(true)
+        print("textFieldShouldReturn")
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        print("textFieldShouldClear")
+        return true
+    }
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        searchText.endEditing(true)
+        searchTextEntered(searchText)
+    }
+}
+
