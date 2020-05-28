b3fbfdc71e0c917e3645db42cb0ae3790003a062
Bikash Das <das.bikash.dev@gmail.com>
2020-05-28 12:22:42 +0600

    Weather app completed with tutorial.

--- Clima/Clima/Controller/WeatherViewController.swift
+++ Clima/Clima/Controller/WeatherViewController.swift
@@ -1,21 +1,98 @@
 import UIKit
+import CoreLocation
 
-class WeatherViewController: UIViewController,UITextFieldDelegate {
-
+class WeatherViewController: UIViewController {
+    
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
     @IBOutlet weak var searchText: UITextField!
+    
+    var weatherManager : WeatherManager? = nil
+    let locationManager = CLLocationManager()
+    let geoCoder = CLGeocoder()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         searchText.delegate = self
+        weatherManager = WeatherManager(delegate: self)
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
     }
+    
+    @IBAction func updateLocation(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+}
 
+//MARK: - CLLocationManagerDelegate
+extension WeatherViewController:CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Got location!")
+        for location in locations{
+            print(location)
+            geocode(location: location, completion: {
+                (placemark:[CLPlacemark]?,error: Error?) in
+                if placemark != nil{
+                    for i in placemark!{
+                        print(i.name)
+                        print(i.postalCode)
+                        print(i.country)
+                        print(i.administrativeArea)
+                        print(i.locality)
+                    }
+                }
+                if error != nil{
+                    print(error)
+                }
+            })
+            self.weatherManager?.fetchWeather(lat: location.coordinate.latitude, lon: location.coordinate.longitude)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    func geocode(location:CLLocation, completion: @escaping (_ placemark: [CLPlacemark]?, _ error: Error?) -> Void)  {
+        self
+            .geoCoder
+            .reverseGeocodeLocation(
+                CLLocation(latitude: location.coordinate.latitude, longitude:  location.coordinate.longitude),
+                completionHandler: completion
+        )
+    }
+}
+
+//MARK: - WeatherUpdateHandler
+
+extension WeatherViewController: WeatherUpdateHandler {
+    
+    func didFailWithError(with error: Error) {
+        print("From WeatherViewController : \(error)")
+    }
+    
+    func didUpdateWeather(with weather: WeatherModel) {
+        print("From WeatherViewController : \(weather)")
+        runOnMainThread {
+            self.temperatureLabel.text = weather.temperatueString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
+        }
+    }
+}
+
+//MARK: - UITextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate{
+    
     @IBAction func searchTextEntered(_ sender: UITextField) {
-        print("searchTextEntered")
-        print(searchText.text!)
+        //        print("searchTextEntered")
+        //        print(searchText.text!)
+        weatherManager?.fetchWeather(cityName: searchText.text!)
+        searchText.text = ""
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
@@ -39,7 +116,12 @@ class WeatherViewController: UIViewController,UITextFieldDelegate {
     
     @IBAction func searchButtonPressed(_ sender: UIButton) {
         searchText.endEditing(true)
-        searchTextEntered(searchText)
+        //        searchTextEntered(searchText)
     }
 }
 
+extension UIViewController{
+    func runOnMainThread(task:@escaping ()->Void){
+        DispatchQueue.main.async{task()}
+    }
+}
